43c8a6bf45628254fb38c9a8439c1f539a953f9b
Daniel Dreke <daniel.dreke@gmx.de>
2019-01-11 03:32:20 +0100

    clean up code (remove unused/outdated functions)

--- assets/js/dynamic_content.js
+++ assets/js/dynamic_content.js
@@ -4,36 +4,6 @@ var htmlcodecached = [];
 var imagespreloaded = [];
 var lastselectedlistname = '';
 
-function backupDefaultList() {
-  var markdowncode = document.getElementById("list_").innerHTML;
-  htmlcodecached["Startauswahl.md"] = marked(markdowncode); 
-  document.getElementById("list_").innerHTML = htmlcodecached["Startauswahl.md"];
-  document.getElementById("list_").style.visibility = "visible";
-}
-
-// source 1: https://www.w3schools.com/xml/tryit.asp?filename=tryajax_first
-// source 2: https://github.com/markedjs/marked
-function preloadMarkdownFile(markdownfilename) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var markdowncode = this.responseText;
-      htmlcode = marked(markdowncode);
-      htmlcode = htmlcode.replace(/\.md/g, '.html'); // fix links
-      htmlcodecached[markdownfilename] = htmlcode;
-    }
-  };
-  xhttp.open("GET", markdownfilename, true);
-  xhttp.send();
-}
-
-function preloadLists() {
-  for (let filenameprefix of filenameprefixes) {
-    var markdownfilename = filenameprefix + '.md';
-    preloadMarkdownFile(markdownfilename);
-  }
-}
-
 function preloadImage(imagenameprefix) {
   var imagename_default = imagenameprefix + '.png';
   var imagename_selected = imagenameprefix + '_selected.png';
@@ -50,35 +20,6 @@ function preloadImages() {
   for (let imagenameprefix of imagenameprefixes) {
     preloadImage(imagenameprefix);
   }
-}
-
-function updateList(userselection = "Startauswahl") {
-  var markdownfilename =  userselection + ".md";
-  var htmlcode = htmlcodecached[markdownfilename];
-  document.getElementById("list_").innerHTML = htmlcode;
-}
-
-function updateSelection(elem) {
-  var userselection = elem.id;
-
-  for (let imagenameprefix of imagenameprefixes) {
-    if (userselection.indexOf(imagenameprefix) == -1) {
-      document.getElementById(imagenameprefix).src = "images/startpage/" + imagenameprefix + ".png";
-      document.getElementById(imagenameprefix).value = "0";
-    } 
-    else {
-      if (elem.value == null || elem.value.valueOf() == "0".valueOf()) {
-        elem.src = "images/startpage/" + imagenameprefix + "_selected.png";
-        elem.value = "1";
-      } 
-      else {
-        elem.src = "images/startpage/" + imagenameprefix + ".png";
-        elem.value = "0";
-        userselection = "Startauswahl";
-      }
-    }
-  }
-  updateList(userselection);
 }
 
 function updateImages(selectedimageelem) {
