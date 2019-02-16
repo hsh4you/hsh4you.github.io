379f18db7ba0561fa6d3d8673d3fb7ad991d328d
Daniel Dreke <daniel.dreke@gmx.de>
2019-02-16 18:09:56 +0530

    add links to google maps for non weekly events

--- calendar/nonweeklyevents.js
+++ calendar/nonweeklyevents.js
@@ -1,19 +1,23 @@
 window.nonweeklyevents = [
   {
     title: 'Erste Hilfe Kurs am Kind im FZ Grashalm (Anmeldung nötig)',
+    url:   'https://www.google.com/maps/?q=FZ Grashalm Berlin',
     start: '2019-03-27'
   }, 
   {
     title: 'Fachtagung der Berliner Jungs',
+    url:   'https://www.google.com/maps/?q=Berliner Jungs',
     start: '2019-03-15',
     end:   '2019-03-16'
   }, 
   {
     title: 'Kiezfest am Malchower See – unter dem Motto „Fete de la Musique“',
+    url:   'https://www.google.com/maps/?q=Malchower See Berlin',
     start: '2019-06-21'
   }, 
   {
     title: 'Malchower Weihnachtsmarkt auf dem Gelände der Naturschutzstation',
+    url:   'https://www.google.com/maps/?q=Naturschutzstation Malchow Berlin',
     start: '2019-12-09'
   }
 ]
