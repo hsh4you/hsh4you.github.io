3d61bb91c8cfce3ccf092f35b8e914bc32f82c88
Daniel Dreke <daniel.dreke@gmx.de>
2019-06-01 00:19:02 +0530

    fix calendar link

--- calendar/nonweeklyevents.js
+++ calendar/nonweeklyevents.js
@@ -1,7 +1,7 @@
 window.nonweeklyevents = [
   {
     title: "Kleidertauschbörse - Zingster Str. 6",
-      url: "https://www.hsh4you.de/Beratung/Beratung_Jugendlicher/Comes.md",
+      url: "https://www.hsh4you.de/Beratung/Beratung_Jugendlicher/Comes.html",
     start: '2019-02-26T15:30',
       end: '2019-02-26T17:30'
   },
