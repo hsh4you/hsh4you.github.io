b14f089578ab2a012b8f53ca025cd116ff197077
DayanaSPIK <43848886+DayanaSPIK@users.noreply.github.com>
2019-06-05 14:28:03 +0200

    Update nonweeklyevents.js

--- calendar/nonweeklyevents.js
+++ calendar/nonweeklyevents.js
@@ -21,13 +21,13 @@ window.nonweeklyevents = [
     start: '2019-03-27'
   },
   {
-    title: "Film „Nachhall - Erinnerungen an die Gehrenseestraße“ im BENN Büro, Warnitzer Str. 14",
+    title: "Fastenbrechen Hsh auf der Wiese vor dem BENN Büro",
       url: "https://www.benn-hohenschoenhausen.de/",
     start: '2019-06-06T19:00',
       end: '2019-06-06T21:00'
   },
   {
-    title: "Fastenbrechen Hsh auf der Wiese vor dem BENN Büro",
+    title: "Film „Nachhall - Erinnerungen an die Gehrenseestraße“ im BENN Büro, Warnitzer Str. 14",
       url: "https://www.benn-hohenschoenhausen.de/",
     start: '2019-06-14T16:00',
       end: '2019-06-14T20:00'
