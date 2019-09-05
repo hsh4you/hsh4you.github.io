02c76930f5d27d964277304ebea7ea882d9cbd31
Daniel Dreke <daniel.dreke@gmx.de>
2019-09-05 17:15:29 +0200

    fix OCB event dates

--- calendar/nonweeklyevents.js
+++ calendar/nonweeklyevents.js
@@ -11,31 +11,6 @@ window.nonweeklyevents = [
       end: '2019-09-12T13:00'
   },
   {
-    title: "Theaterprojekt in den Herbstferien @ OCB",
-      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
-    start: '2019-09-14'
-  },
-  {
-    title: "Theaterprojekt in den Herbstferien @ OCB",
-      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
-    start: '2019-09-15'
-  },
-  {
-    title: "Theaterprojekt in den Herbstferien @ OCB",
-      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
-    start: '2019-09-16'
-  },
-  {
-    title: "Theaterprojekt in den Herbstferien @ OCB",
-      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
-    start: '2019-09-17'
-  },
-  {
-    title: "Theaterprojekt in den Herbstferien @ OCB",
-      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
-    start: '2019-09-18'
-  },
-  {
     title: "„We will rock you 2.0“ Fest @ JFE Mikado",
       url: "https://www.hsh4you.de/Jugendklubs/Mikado.html",
     start: '2019-09-20'
@@ -44,6 +19,31 @@ window.nonweeklyevents = [
     title: "Laternenumzug im Kiez @ OCB",
       url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
     start: '2019-10-05'
+  },
+  {
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-10-14'
+  },
+  {
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-10-15'
+  },
+  {
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-10-16'
+  },
+  {
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-10-17'
+  },
+  {
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-10-18'
   },
   {
     title: "Stand auf dem Lichtermarkt @ OCB",
