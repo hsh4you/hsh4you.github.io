32a5721182b69f077fb50a46a540d47d17c1ba7d
Daniel Dreke <daniel.dreke@gmx.de>
2019-09-05 15:26:15 +0200

    remove outdated events from calendar and add OCB events

--- calendar/nonweeklyevents.js
+++ calendar/nonweeklyevents.js
@@ -1,94 +1,63 @@
 window.nonweeklyevents = [
   {
-    title: "Kleidertauschbörse - Zingster Str. 6",
-      url: "https://www.hsh4you.de/Beratung/Beratung_Jugendlicher/Comes.html",
-    start: '2019-02-26T15:30',
-      end: '2019-02-26T17:30'
+    title: "Mädchen*aktionstag @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-09-12'
   },
   {
-    title: "Fachtagung der Berliner Jungs",
-      url: "https://www.hsh4you.de/Beratung/Beratung_Jugendlicher/Berliner_Jungs.html",
-    start: '2019-03-15'
-  },
-  {
-    title: "Fachtagung der Berliner Jungs",
-      url: "https://www.hsh4you.de/Beratung/Beratung_Jugendlicher/Berliner_Jungs.html",
-    start: '2019-03-16'
-  },
-  {
-    title: "Erste Hilfe Kurs am Kind im FZ Grashalm (Anmeldung nötig)",
-      url: "https://www.google.com/maps/?q=FZ Grashalm Berlin",
-    start: '2019-03-27'
-  },
-  {
-    title: "Fastenbrechen Hsh auf der Wiese vor dem BENN Büro",
-      url: "https://www.benn-hohenschoenhausen.de/",
-    start: '2019-06-06T16:00',
-      end: '2019-06-06T20:00'
-  },
-  {
-    title: "Film „Nachhall - Erinnerungen an die Gehrenseestraße“ im BENN Büro, Warnitzer Str. 14",
-      url: "https://www.benn-hohenschoenhausen.de/",
-    start: '2019-06-14T19:00',
-      end: '2019-06-14T21:00'
-  },
-  {
-    title: "25. Storchenfest in der Naturschutzstation Malchow",
-      url: "http://www.naturschutzstation-malchow.de/index.php/aktuelles/termine",
-    start: '2019-06-15T12:00',
-      end: '2019-06-15T19:00'
-  },
-  {
-    title: "25. Storchenfest in der Naturschutzstation Malchow",
-      url: "http://www.naturschutzstation-malchow.de/index.php/aktuelles/termine",
-    start: '2019-06-16T11:00',
-      end: '2019-06-16T13:00'
-  },
-  {
-    title: "Kiezrallye mit Action Bound „Roll in die Ferien“-Start @ JFE Welseclub",
-      url: "https://www.hsh4you.de/Jugendklubs/jfe_welseclub.html",
-    start: '2019-06-19'
-  },
-  {
-    title: "Kiezfest am Malchower See als Teil von „Fete de la Musique“",
-      url: "https://www.google.com/maps/?q=Malchower See Berlin",
-    start: '2019-06-21T15:00',
-      end: '2019-06-21T20:00'
-  },
-  {
-    title: "Kleidertausch 1xMonat Sommerspecial mit Kuchen @ Comes e.V., Zingster Str.6.",
-      url: "https://www.hsh4you.de/Beratung/Beratung_Jugendlicher/Comes.html",
-    start: '2019-06-25'
-  },
-  {
-    title: "VIPstival in der JFE VIP",
-      url: "https://www.hsh4you.de/Jugendklubs/Kontaktladen_VIP.html",
-    start: '2019-08-01'
-  },
-  {
-    title: "Jungenaktionstag in den Krugwiesen von Berliner Jungs",
-      url: "https://www.hsh4you.de/Beratung/Beratung_Jugendlicher/Berliner_Jungs.html",
-    start: '2019-08-09'
-  },
-  {
-    title: "Aktionstag für Alleinerziehende @ VAV Nachbarschaftshaus, Ribnitzer Str. 1b",
-      url: "https://www.google.com/maps/?q=VAV Nachbarschaftshaus Berlin",
-    start: '2019-08-23'
-  },
-  {
-    title: "Nächste Regional AG im JFE Mikado",
+    title: "Nächste Regional AG @ JFE Mikado",
       url: "https://www.hsh4you.de/Jugendklubs/Mikado.html",
     start: '2019-09-12T10:30',
       end: '2019-09-12T13:00'
   },
   {
-    title: "„We will rock you 2.0“ Fest im JFE Mikado",
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-09-14'
+  },
+  {
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-09-15'
+  },
+  {
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-09-16'
+  },
+  {
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-09-17'
+  },
+  {
+    title: "Theaterprojekt in den Herbstferien @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-09-18'
+  },
+  {
+    title: "„We will rock you 2.0“ Fest @ JFE Mikado",
       url: "https://www.hsh4you.de/Jugendklubs/Mikado.html",
     start: '2019-09-20'
+  },
+  {
+    title: "Laternenumzug im Kiez @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-10-05'
+  },
+  {
+    title: "Stand auf dem Lichtermarkt @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-12-01'
   },
   {
     title: "Malchower Weihnachtsmarkt auf dem Gelände der Naturschutzstation",
       url: "http://www.naturschutzstation-malchow.de/index.php/aktuelles/termine",
     start: '2019-12-09'
+  },
+  {
+    title: "Winterfest @ OCB",
+      url: "https://www.hsh4you.de/Jugendklubs/OCB.html",
+    start: '2019-12-20'
   }
 ]
